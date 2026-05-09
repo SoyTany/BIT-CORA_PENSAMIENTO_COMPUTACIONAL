d95b6b003ba29962e6870e012e3b31b5ae0da1b6
SoyTany <tanyvic.vergaraobr@gmail.com>
2026-05-09 01:26:03 -0400

    Guardado 1

---
+++ Entregas/Encargo-05.js
@@ -0,0 +1,2 @@
+//Entrega 05, Unidad 2
+//Encargo: Hacer que formas primitivas varíen en el tiempo, cambiando características basandose en la realización de comportamientos variables. **Implementar 3 de ellas**.
