bb1e58cfcd5ed5a9e5d9beefaf462383aa08db7f
SoyTany <tanyvic.vergaraobr@gmail.com>
2026-05-15 19:13:38 -0400

    Añadido

---
+++ Entregas/Encargo-02.js
@@ -0,0 +1,51 @@
+//Enlace: https://editor.p5js.org/Tanytanita/sketches/vHSxZLBlZ
+
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background("#FFFAE0");
+  fill("#F0D271")
+  stroke("#F0D271")
+  strokeWeight(1)
+  circle(256, 450, 210);
+  stroke("#F0D271")
+  strokeWeight(1)
+  circle(110, 315, 100);
+  fill("#944B8F")
+  stroke("#944B8F")
+  strokeWeight(1)
+  triangle(40, 410, 130, 305, 130, 410);
+  fill("#FFFAE0")
+  stroke("#000000")
+  strokeWeight(2)
+  triangle(90, 386, 178, 280, 178, 386);
+  line(95, 380, 178, 380);
+  line(100, 374, 178, 374);
+  line(105, 368, 178, 368);
+  line(110, 362, 178, 362);
+  line(115, 356, 178, 356);
+  line(120, 350, 178, 350);
+  line(125, 344, 178, 344);
+  line(130, 338, 178, 338);
+  line(135, 332, 178, 332);
+  line(140, 326, 178, 326);
+  line(145, 320, 178, 320);
+  line(150, 314, 178, 314);
+  line(155, 308, 178, 308);
+  line(160, 302, 178, 302);
+  line(165, 296, 178, 296);
+  line(170, 290, 178, 290);
+  stroke("#000000")
+  strokeWeight(7)
+  strokeCap(SQUARE)
+  line(340, 130, 340, 430);
+  line(320, 130, 320, 346);
+  line(300, 130, 300, 280);
+  fill("#944B8F")
+  stroke("#000000")
+  strokeWeight(2)
+  rect(130, 50, 232, 90);
+  
+  }
